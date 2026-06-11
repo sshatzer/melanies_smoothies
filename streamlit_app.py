59ea082df9d3d0cb6dbfa1bc4ee3244a343950fd
sshatzer <72048245+sshatzer@users.noreply.github.com>
2026-06-11 12:14:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests 
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -39,6 +40,5 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
